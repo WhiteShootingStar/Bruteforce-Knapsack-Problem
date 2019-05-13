d91a5ca12ab7e17c29314834b92c1923779cbfd2
Golbez <palart2000@gmail.com>
2019-05-13 21:14:42 +0200

    optimisztion?

--- src/knapsackExecution/Bruteforce.java
+++ src/knapsackExecution/Bruteforce.java
@@ -17,28 +17,36 @@ public class Bruteforce {
 
 	public void classify(String filepath) {
 		try {
-			Map<List<Item>, Integer> map = new HashMap<>();
+			
 			final int KNAPSACKSIZE = Utilities.getKnapsackSize(filepath);
 			final List<Item> ITEMS = Utilities.getItemsList(filepath);
 			long start = System.currentTimeMillis();
-			for (int i = 0; i < Math.pow(2, ITEMS.size()); i++) {
+			List<Item> solution = null;
+			int bestSum = -1;
+			for (int i = 0; i < Math.pow(2, ITEMS.size()-1); i++) {
 				int[] characteristicVector = getCharVector(ITEMS, i);
 				List<Item> currentItems = extractItems(characteristicVector, ITEMS);
-				if (doesntExceedCapacityItems(KNAPSACKSIZE, currentItems)) {
-					int sum = getSumOfValues(currentItems);
-					map.put(currentItems, sum);
+				if (solution == null && doesntExceedCapacityItems(KNAPSACKSIZE, currentItems)) {
+					solution = currentItems;
+					bestSum = getSumOfValues(solution);
+				} else if (doesntExceedCapacityItems(KNAPSACKSIZE, currentItems)) {
 
-				//System.out.println(currentItems);
+					int sum = getSumOfValues(currentItems);
+					if (sum > bestSum) {
+						solution = currentItems;
+						bestSum = sum;
+					}
+
+					
 				}
+				System.out.println(i);
 			}
-			int maxSum = getMaxSum(map.values());
-			List<List<Item>> answers = getAnswers(map, maxSum);
-			for (List<Item> list : answers) {
-				System.out.println(getSumOfValues(list) + " <----------- VALUES");
-				System.out.println(getSumOfWeights(list)+ " <------------WEIGHTS");
-				System.out.println(Arrays.toString(transformBack(list, ITEMS.size())));
-				System.out.println(list + "  <---------BEST OPTION");
-			}
+
+			System.out.println(getSumOfValues(solution) + " <----------- VALUES");
+			System.out.println(getSumOfWeights(solution) + " <------------WEIGHTS");
+			System.out.println(Arrays.toString(transformBack(solution, ITEMS.size())));
+			System.out.println(solution + "  <---------BEST OPTION");
+
 			long finish = System.currentTimeMillis();
 			System.out.println("DONE in " + (finish - start) + " miliseconds");
 		} catch (IOException e) {
@@ -50,9 +58,20 @@ public class Bruteforce {
 	private int[] getCharVector(List<Item> list, int number) {
 		int[] vector = new int[list.size()];
 		int temp = number;
+
 		for (int j = vector.length - 1; j >= 0; j--) {
 			vector[j] = temp % 2;
 			temp = temp / 2;
+		}
+		return vector;
+	}
+
+	private int[] getCharVectorNEW(List<Item> list, int number) {
+		int[] vector = new int[list.size()];
+		int temp = number;
+		String[] val = Integer.toBinaryString(temp).split("");
+		for (int i = val.length - 1; i >= 0; i--) {
+			vector[i] = Integer.parseInt(val[i]);
 		}
 		return vector;
 	}
@@ -69,6 +88,7 @@ public class Bruteforce {
 	private int getSumOfWeights(List<Item> list) {
 		return list.stream().mapToInt(e -> e.weight).sum();
 	}
+
 	private boolean doesntExceedCapacityItems(int capacity, List<Item> list) {
 
 		return getSumOfWeights(list) <= capacity;
@@ -81,9 +101,10 @@ public class Bruteforce {
 	private List<List<Item>> getAnswers(Map<List<Item>, Integer> map, int maxSum) {
 		return map.keySet().stream().filter(e -> getSumOfValues(e) == maxSum).collect(Collectors.toList());
 	}
+
 	private int[] transformBack(List<Item> list, int size) {
 		int[] charVector = new int[size];
-		list.stream().mapToInt(e->e.currentID).forEachOrdered(e->charVector[e]=1);
+		list.stream().mapToInt(e -> e.currentID).forEachOrdered(e -> charVector[e] = 1);
 		return charVector;
 	}
 }
